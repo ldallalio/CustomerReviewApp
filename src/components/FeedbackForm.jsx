c8fa8427ce4e49378695dff864b7c4929db891b7
Logan Dallalio <logan@dallalioweb.dev>
2021-12-17 05:01:32 -0600

    AddingRating

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,27 +1,46 @@
 import {useState} from 'react'
 import Card from "./shared/Card"
+import Button from './shared/Button'
+import { div } from 'prelude-ls'
+import RatingSelect from './RatingSelect'
 
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [rating, setRating] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (event) => {
-        setText(event.target.value);
+        if(text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 10) {
+            setMessage('Text Must Be Atleast 10 Characters')
+            setBtnDisabled(true)
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+        setText(event.target.value)
     }
 
     return (
         <Card>
             <form>
                 <h2>How would you rate your service with us?</h2>
-                {/* @Todo - rating select componet*/}
+                <RatingSelect select={(rating) => setRating(rating)} />
                 <div className="input-group">
                     <input onChange={handleTextChange} 
                     type="text" 
                     placeholder='Write a review'
                      value={text}
                      />
-                    <button type="submit">Send</button>
+                    <Button type="submit" isDisabled={btnDisabled}>Send</Button>
                 </div>
+
+                {message && <div className="message">{message}</div>}
+
             </form>
         </Card>
     )
