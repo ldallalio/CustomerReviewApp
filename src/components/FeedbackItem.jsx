9556435cc30d2c07d324750b4a8607ce805d63e6
Logan Dallalio <logan@dallalioweb.dev>
2021-12-17 03:04:09 -0600

    Creating Form for App

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,18 +2,13 @@ import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from "./shared/Card"
 
-function FeedbackItem({ item }) {
-
-    const handleClick = (id) => {
-        console.log(id)
-    }
-
+function FeedbackItem({ item, handleDelete }) {
 
 
     return (
         <Card >
             <div className="num-display">{item.rating}</div>
-            <button className='close' onClick={() => {handleClick(item.id)}} >
+            <button className='close' onClick={() => {handleDelete(item.id)}} >
             <FaTimes color='purple'/>
             </button>
             <div className="text-display">{item.text}</div>
