3dc1a9b12a6f9f6d8d9c2afe783bf8c0c78adfac
Logan Dallalio <logan@dallalioweb.dev>
2021-12-17 01:20:32 -0600

    Adding Items and Conditionals

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,28 @@
+import {FaTimes} from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Card from "./shared/Card"
+
 function FeedbackItem({ item }) {
+
+    const handleClick = (id) => {
+        console.log(id)
+    }
+
+
+
     return (
-        <div className='card'>
+        <Card >
             <div className="num-display">{item.rating}</div>
+            <button className='close' onClick={() => {handleClick(item.id)}} >
+            <FaTimes color='purple'/>
+            </button>
             <div className="text-display">{item.text}</div>
-        </div>
+        </Card>
     )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+function Card({children, reverse}) {
+    /* return (
+       <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+    ) */
+    return (
+        <div className="card" style={{
+            backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#ffff',
+            color: reverse ? '#ffff' : '#000',
+        }}>{children}</div>
+    )
+}
+
+Card.defaultProps = {
+    reverse: false,
+}
+
+Card.propType = {
+    children: PropTypes.node.isRequired,
+    reverse: PropTypes.bool
+}
+
+export default Card
