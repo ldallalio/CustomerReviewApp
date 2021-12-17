bb76ff108c528ef046898f69e0c2e886ee419dbc
Logan Dallalio <logan@dallalioweb.dev>
2021-12-17 05:59:17 -0600

    React Router

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,20 @@
+import Card from "../components/shared/Card"
+
+function AboutPage() {
+    return (
+        <Card>
+            <div>
+                 <h1>About This Project</h1>
+                 <p>This is an awesome app to allow for customer reviews!</p>
+                 <p>Version: 1.0.0</p>
+
+                 <p>
+                     <a href="/">Back To Home</a>
+                 </p>
+            </div>
+        </Card>
+        
+    )
+}
+
+export default AboutPage
