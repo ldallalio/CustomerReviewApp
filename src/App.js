62573aec249927e367c924bdc5fe0f6538101611
Logan Dallalio <logan@dallalioweb.dev>
2021-12-17 00:15:40 -0600

    Learning State with React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-function App () {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'}
-    ]
+import {useState} from 'react'
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from './data/FeedbackData'
 
-    return (
-        <div className="container">
-            <h1>{title}</h1>
-            <p>{body}</p>
-            
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map((comment, index) => (
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-        
-    )
+function App () {
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+   return (
+       <>
+        <Header />
+
+       <div className="container">
+           <FeedbackList feedback={feedback} />
+       </div>
+       </>
+   )
 }
 
 export default App
