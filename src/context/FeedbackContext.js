5c19afb63a1d71b68a82e975dbef269cb4a29536
Logan Dallalio <logan@dallalioweb.dev>
2021-12-18 00:13:28 -0600

    Setting up and using JSON Server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,33 @@
 //This is where state goes
 import { v4 as uuidv4} from 'uuid'
-import {createContext, useState} from 'react'
+import {createContext, useState, useEffect} from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
+    //State
     const [feedback, setFeedback] = useState([
-        {
-            id:1,
-            text: 'This item is from context #1',
-            rating:1
-        },
-        {
-            id:2,
-            text: 'This item is from context #2',
-            rating:5
-        },
-        {
-            id:4,
-            text: 'This item is from context #3',
-            rating:9
-        },
+
     ])
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    //Fetch Feedback -- Getting Feedback from JSON Server
+    const fetchFeedback = async () => {
+        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+        const data = await response.json()
+
+        setFeedback(data)
+        setIsLoading(false)
+    }
+
     //Set Item to be updated
     const editFeedback = (item) => {
         setFeedbackEdit({
@@ -44,15 +46,14 @@ export const FeedbackProvider = ({children}) => {
         newFeedback.id = uuidv4()
         setFeedback([newFeedback, ...feedback])
     }
-
     //Update Feedback Item
     const updateFeedback = (id, updItem) => {
         setFeedback(feedback.map((item) => item.id === id ? {... item, ...updItem} : item))
     }
-
     return <FeedbackContext.Provider value={{
         feedback,
         feedbackEdit, ///Actual Item to Edit
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback, //This is the function
