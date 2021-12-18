d269e0bfa45321e64cf67cc940c3f9a60c3f53cf
Logan Dallalio <logan@dallalioweb.dev>
2021-12-17 22:49:02 -0600

    Continuing React Contexgt

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,2 +1,23 @@
 //This is where state goes
 
+import {createContext, useState} from 'react'
+
+const FeedbackContext = createContext()
+
+export const FeedbackProvider = ({children}) => {
+    const [feedback, setFeedback] = useState([
+        {
+            id:1,
+            text: 'This item is from context',
+            rating:10
+        }
+    ])
+    return <FeedbackContext.Provider value={{
+        feedback,
+    }}>
+        {children}
+    </FeedbackContext.Provider>
+}
+
+
+export default FeedbackContext
